04c194acfb4f1af835cc99db813090d92d849eba
ZeX <165585539+ddosnotification@users.noreply.github.com>
2024-11-22 11:02:58 +0100

    Rewrite of whitelist.sh
    Changing level of network stack to lower using mangle.

--- whitelist.sh
+++ whitelist.sh
@@ -5,17 +5,25 @@ ipv4_ranges=(
     "15.204.104.217"
 )
 
-# Clear existing rules (optional)
-# iptables -F
-# ip6tables -F
+# Clear existing mangle rules (optional)
+# iptables -t mangle -F
+# ip6tables -t mangle -F
 
-# Add IPv4 whitelist rules
-for ip in "${ipv4_addresses[@]}"; do
-    iptables -I INPUT -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT
+# Allow established connections first
+iptables -t mangle -A PREROUTING -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -t mangle -A PREROUTING -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# Add IPv4 whitelist rules in mangle table
+for ip in "${ipv4_ranges[@]}"; do
+    iptables -t mangle -A PREROUTING -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT
 done
 
-# Block all other IPs for HTTP/HTTPS
-iptables -A INPUT -p tcp -m multiport --dports http,https -j DROP
-ip6tables -A INPUT -p tcp -m multiport --dports http,https -j DROP
+# Block all other IPs for HTTP/HTTPS at PREROUTING
+iptables -t mangle -A PREROUTING -p tcp -m multiport --dports http,https -j DROP
+ip6tables -t mangle -A PREROUTING -p tcp -m multiport --dports http,https -j DROP
 
-echo "Firewall rules have been updated successfully!"
+# Add logging for dropped packets (optional)
+iptables -t mangle -A PREROUTING -p tcp -m multiport --dports http,https -j LOG --log-prefix "HTTP/HTTPS DROP: " --log-level 6
+ip6tables -t mangle -A PREROUTING -p tcp -m multiport --dports http,https -j LOG --log-prefix "HTTP/HTTPS DROP: " --log-level 6
+
+echo "Mangle PREROUTING rules have been updated successfully!"
